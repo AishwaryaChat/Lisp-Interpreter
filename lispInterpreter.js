5bab653ca19fc5fa80cfd06e2de8974fbf90f058
AishwaryaChat <chaturvedi.aishwarya@gmail.com>
2017-05-29 12:30:02 +0530

    Modify plus parser

--- lispInterpreter.js
+++ lispInterpreter.js
@@ -1,4 +1,4 @@
-const plusParser = (input) => input.startsWith('+') ? ['+', input.slice(1)] : null
+const plusParser = (input) => input.startsWith('+') ? [plus, input.slice(1)] : null
 const spaceParser = (input) => input.match(/^[\s\n]/) ? [null, input.slice(input.match(/\S/).index)] : null
 const  numParser = (input) => {
   let regexp = String(input).match(/^[-+]?(\d+(\.\d*)?|\.\d+)([e][+-]?\d+)?/)
@@ -6,6 +6,20 @@ const  numParser = (input) => {
   return [parseInt(regexp[0]), input.slice(regexp[0].length)]
 }
 
-let input = '1'
-let output = numParser(input)
+const plus = (a, b) => {
+  return a + b
+}
+
+let result = []
+let input = '+ 1 2'
+let output = plusParser(input)
+result.push(output[0])
+output = spaceParser(output[1])
+output = numParser(output[1])
+result.push(output[0])
+output = spaceParser(output[1])
+output = numParser(output[1])
+result.push(output[0])
+const fun = result.shift()
+output = fun(...result)
 console.log(output)
