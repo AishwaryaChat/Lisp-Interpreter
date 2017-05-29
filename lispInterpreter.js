755572a09eac7895e9564dd00cc3d1f25d58fb08
AishwaryaChat <chaturvedi.aishwarya@gmail.com>
2017-05-29 13:01:00 +0530

    Add minus parser

--- lispInterpreter.js
+++ lispInterpreter.js
@@ -1,6 +1,7 @@
-const plusParser = (input) => input.startsWith('+') ? [plus, input.slice(1)] : null
-const spaceParser = (input) => input.match(/^[\s\n]/) ? [null, input.slice(input.match(/\S/).index)] : null
-const  numParser = (input) => {
+const plusParser = input => input.startsWith('+') ? [plus, input.slice(1)] : null
+const minusParser = input => input.startsWith('-') ? [minus, input.slice(1)] : null
+const spaceParser = input => input.match(/^[\s\n]/) ? [null, input.slice(input.match(/\S/).index)] : null
+const  numParser = input => {
   let regexp = String(input).match(/^[-+]?(\d+(\.\d*)?|\.\d+)([e][+-]?\d+)?/)
   if (!String(input).match(/^[-+]?(\d+(\.\d*)?|\.\d+)([e][+-]?\d+)?/)) return null
   return [parseInt(regexp[0]), input.slice(regexp[0].length)]
@@ -10,9 +11,13 @@ const plus = (a, b) => {
   return a + b
 }
 
+const minus = (a, b) => {
+  return a - b
+}
+
 let result = []
-let input = '+ 1 2'
-let output = plusParser(input)
+let input = '- 10 8'
+let output = minusParser(input)
 result.push(output[0])
 output = spaceParser(output[1])
 output = numParser(output[1])
