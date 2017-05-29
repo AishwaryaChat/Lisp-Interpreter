ddc74a0790ccf87b639872e53ce25045b5121525
AishwaryaChat <chaturvedi.aishwarya@gmail.com>
2017-05-29 13:07:50 +0530

    Add division Parser

--- lispInterpreter.js
+++ lispInterpreter.js
@@ -1,6 +1,7 @@
 const plusParser = input => input.startsWith('+') ? [plus, input.slice(1)] : null
 const minusParser = input => input.startsWith('-') ? [minus, input.slice(1)] : null
 const multiplyParser = input => input.startsWith('*') ? [multiply, input.slice(1)] : null
+const divideParser = input => input.startsWith('/') ? [divide, input.slice(1)] : null
 const spaceParser = input => input.match(/^[\s\n]/) ? [null, input.slice(input.match(/\S/).index)] : null
 const  numParser = input => {
   let regexp = String(input).match(/^[-+]?(\d+(\.\d*)?|\.\d+)([e][+-]?\d+)?/)
@@ -20,9 +21,17 @@ const multiply = (a, b) => {
   return a * b
 }
 
+const divide = (a, b) => {
+  if (b === 0) {
+    return 'Can not divide by zero'
+  } else {
+    return a / b
+  }
+}
+
 let result = []
-let input = '* 10 8'
-let output = multiplyParser(input)
+let input = '/ 10 10'
+let output = divideParser(input)
 result.push(output[0])
 output = spaceParser(output[1])
 output = numParser(output[1])
