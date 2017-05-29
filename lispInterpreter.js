3d302f85aa1014116ec5ac8e95c6415ab25832ee
AishwaryaChat <chaturvedi.aishwarya@gmail.com>
2017-05-29 16:22:25 +0530

    Add the following: 1. define parser 2. Identifier Parser

--- lispInterpreter.js
+++ lispInterpreter.js
@@ -4,6 +4,7 @@ const numParser = input => {
   if (!String(input).match(/^[-+]?(\d+(\.\d*)?|\.\d+)([e][+-]?\d+)?/)) return null
   return [parseInt(regexp[0]), input.slice(regexp[0].length)]
 }
+const identifierParser = input => input.match(/\w+/) ? [input.match(/\w+/)[0], input.slice(input.match(/\s+/).index)] : null
 
 const plusParser = input => input.startsWith('+') ? [plus, input.slice(1)] : null
 const minusParser = input => input.startsWith('-') ? [minus, input.slice(1)] : null
@@ -11,9 +12,10 @@ const multiplyParser = input => input.startsWith('*') ? [multiply, input.slice(1
 const divideParser = input => input.startsWith('/') ? [divide, input.slice(1)] : null
 const gtParser = input => input.startsWith('>') ? [greaterThan, input.slice(1)] : null
 const ltParser = input => input.startsWith('<') ? [lessThan, input.slice(1)] : null
-let gteParser = input => input.startsWith('>=') ? [gtEqTo, input.slice(2)] : null
-let lteParser = input => input.startsWith('<=') ? [ltEqTo, input.slice(2)] : null
-let etParser = input => input.startsWith('==') ? [eqTo, input.slice(2)] : null
+const gteParser = input => input.startsWith('>=') ? [gtEqTo, input.slice(2)] : null
+const lteParser = input => input.startsWith('<=') ? [ltEqTo, input.slice(2)] : null
+const etParser = input => input.startsWith('==') ? [eqTo, input.slice(2)] : null
+const defParser = input => input.startsWith('define') ? [def, input.slice(6)] : null
 
 const expressionParser = (input) => {
   let result = []
@@ -21,16 +23,24 @@ const expressionParser = (input) => {
   if (!input.startsWith('(')) return null
   input = input.slice(1)
   while (true) {
-    output = lteParser(input)
+    output = operatorParser(input)
+    if (output === null) return null
     result.push(output[0])
     output = spaceParser(output[1])
-    output = numParser(output[1])
+    output = identifierParser(output[1])
     result.push(output[0])
     output = spaceParser(output[1])
     output = numParser(output[1])
     result.push(output[0])
     if (output[1] === ')') return result
   }
+}
+
+const operatorParser = (input) => {
+  let result
+  result = plusParser(input) || minusParser(input) || multiplyParser(input) || divideParser(input) || gteParser(input) || lteParser(input) ||
+  etParser(input) || gtParser(input) || ltParser(input) || defParser(input) || identifierParser(input)
+  return result
 }
 
 const plus = (a, b) => {
@@ -73,9 +83,17 @@ const eqTo = (a, b) => {
   return a === b
 }
 
+const def = (a, b) => {
+  return a = b
+}
+
 let result = []
-let input = '(<= 10 10)'
+let input = '(define abc 10)'
 let output = expressionParser(input)
-const fun = output.shift()
-output = fun(...output)
+if (output === null) {
+  output = 'error'
+} else {
+  const fun = output.shift()
+  output = fun(...output)
+}
 console.log(output)
