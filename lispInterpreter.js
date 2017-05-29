82987e9c6fd310abbec71fc14468aa156a77c731
AishwaryaChat <chaturvedi.aishwarya@gmail.com>
2017-05-29 12:04:11 +0530

    Add Number Parser

--- lispInterpreter.js
+++ lispInterpreter.js
@@ -1,2 +1,11 @@
 const plusParser = (input) => input.startsWith('+') ? ['+', input.slice(1)] : null
 const spaceParser = (input) => input.match(/^[\s\n]/) ? [null, input.slice(input.match(/\S/).index)] : null
+const  numParser = (input) => {
+  let regexp = String(input).match(/^[-+]?(\d+(\.\d*)?|\.\d+)([e][+-]?\d+)?/)
+  if (!String(input).match(/^[-+]?(\d+(\.\d*)?|\.\d+)([e][+-]?\d+)?/)) return null
+  return [parseInt(regexp[0]), input.slice(regexp[0].length)]
+}
+
+let input = '1'
+let output = numParser(input)
+console.log(output)
