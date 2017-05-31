73a826f0849b99f45591a5f49cc6e9220a94994f
AishwaryaChat <chaturvedi.aishwarya@gmail.com>
2017-05-31 14:16:15 +0530

    Add Statement Parser to evaluate print statement

--- lispInterpreter.js
+++ lispInterpreter.js
@@ -18,3 +18,37 @@ const identifierParser = input => {
   let idStr = match[0]
   return [idStr, input.slice(idStr.length)]
 }
+
+const statementParser = (input) => {
+  let output
+  while (true) {
+    if (input.startsWith('(')) {
+      input = input.slice(1)
+      if (input.startsWith('print')) {
+        input = input.slice(6)
+        output = numParser(input) || identifierParser(input)
+        console.log(output[0])
+        input = output[1]
+        if (!output[1].startsWith(')')) return null
+        input = input.slice(1)
+        if (input !== null) {
+          output = spaceParser(input)
+          if (output !== null) {
+            input = output[1]
+          }
+        }
+        if (input === '') break
+      }
+    }
+  }
+}
+
+const printFun = () => {
+
+}
+
+let input = `(print 10)
+(print a)(print 9)
+(print 50)
+(print b)(print 20)`
+let output = statementParser(input)
