43292f43d00f17f70562cf7f845cf94ca57344aa
AishwaryaChat <chaturvedi.aishwarya@gmail.com>
2017-05-31 19:59:32 +0530

    Add the following: 1. Program Parser 2. Re-factor statement parser

--- lispInterpreter.js
+++ lispInterpreter.js
@@ -23,52 +23,62 @@ const identifierParser = input => {
 }
 
 const statementParser = (input) => {
-  if (input.startsWith('(')) {
-    let output = ''
-    input = input.slice(1)
-    while (true) {
-      if (input.startsWith('print')) {
-        input = input.slice(6)
-        output = identifierParser(input)
-        if (output !== null) {
-          let val = ENV[output[0]]
-          if (val === undefined) {
-            console.log('Error ', output[0], ' is not defined')
-            break
-          } else {
-            console.log(val)
-          }
-        } else {
-          output = numParser(input)
-          console.log(output[0])
-        }
-        input = output[1]
-      } else if (input.startsWith('define')) {
-        let result = []
-        input = input.slice(7)
-        output = identifierParser(input)
-        result.push(output[0])
-        input = output[1]
-        output = spaceParser(input)
-        output = numParser(output[1])
-        result.push(output[0])
-        defFun(...result)
-        input = output[1]
-      } else break
+  let output = ''
+  if (input.startsWith('print')) {
+    input = input.slice(6)
+    output = identifierParser(input)
+    if (output !== null) {
+      let val = ENV[output[0]]
+      if (val === undefined) {
+        console.log('Error ', output[0], ' is not defined')
+        return null
+      } else {
+        console.log(val)
+      }
+    } else {
+      output = numParser(input)
+      console.log(output[0])
     }
+    input = output[1]
     if (!input.startsWith(')')) return null
-    input = input.slice(1)
-    if (input !== '') {
-      output = spaceParser(input)
-      if (output !== null) {
-        input = output[1]
-        output = statementParser(input)
-      } else statementParser(input)
-    }
+    return input.slice(1)
+  } else if (input.startsWith('define')) {
+    let result = []
+    input = input.slice(7)
+    output = identifierParser(input)
+    result.push(output[0])
+    input = output[1]
+    output = spaceParser(input)
+    output = numParser(output[1])
+    result.push(output[0])
+    defFun(...result)
+    input = output[1]
+    if (!input.startsWith(')')) return null
+    return input.slice(1)
+  }
+  return null
+}
+
+const programParser = (input) => {
+  while (true) {
+    if (input.startsWith('(')) {
+      input = input.slice(1)
+      let output = ''
+      output = statementParser(input)
+      input = output
+      if (input !== '') {
+        output = spaceParser(input)
+        if (output !== null) {
+          input = output[1]
+        }
+      }
+      if (input === '') return 'completed'
+    } else return null
   }
 }
 
 const defFun = (a, b) => { ENV[a] = b }
 
 let input = fs.readFileSync('test.txt').toString()
-let output = statementParser(input)
+let output = programParser(input)
+console.log(output)
